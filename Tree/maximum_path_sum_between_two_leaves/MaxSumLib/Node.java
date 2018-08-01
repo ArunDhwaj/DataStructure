31aada2669dcc16864611c7c7a1cf1970ad61151
spb.arun.01@gmail.com <sbp@SmartBitPixels-MacBook-Pro.local>
2018-08-01 20:24:16 +0530

    Tree is added

---
+++ Tree/maximum_path_sum_between_two_leaves/MaxSumLib/Node.java
@@ -0,0 +1,15 @@
+package MaxSumLib;
+
+public class Node
+{
+    public int data;
+
+    public Node left = null;
+    public Node right = null;
+
+    public Node(int data)
+    {
+        this.data = data;
+        this.left = this.right = null;
+    }
+}
---
+++ Tree/maximum_path_sum_between_two_leaves/MaxSumLib/Res.java
@@ -0,0 +1,15 @@
+package MaxSumLib;
+
+// An object of Res is passed around so that the
+// same value can be used by multiple recursive calls.
+
+public class Res 
+{
+    public int val;
+    
+    public Res()
+    {
+        
+    }
+}
+
